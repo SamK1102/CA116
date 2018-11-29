22c201e624916a47a98551f7d91198637b75f980
SamK1102 <45040019+SamK1102@users.noreply.github.com>
2018-11-29 15:23:51 +0000

    Add files via upload

---
+++ caesar-encrypt.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import sys
+
+n = 13
+lower = "abcdefghijklmnopqrstuvwxyz"
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+src = lower + upper
+dst = lower[n:] + lower[:n] + upper[n:] + upper[:n]
+
+d = {
+}
+
+i = 0
+while i < len(src):
+    d[src[i]] = dst[i]
+    i = i + 1
+
+f = sys.stdin.read().strip()
+s = []
+i = 0
+while i < len(f):
+    if f[i] not in src:
+        s.append(f[i])
+    else:
+        s.append(d[f[i]])
+    i = i + 1
+print "".join(s)
